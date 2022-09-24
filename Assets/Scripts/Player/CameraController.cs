23e4ac2e054dbb30160cb66a5a595c9cdd8ef0cd
Joel <fallendemongod@gmail.com>
2022-09-24 17:14:42 -0400

    CameraController

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Title("Camera Settings")]
+    [SerializeField]private CinemachineVirtualCamera playerCamera;
+    
+    
+    
+    
+}
