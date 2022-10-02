f1a6b2e8ecb4b1f30e9883b256f9cb83b206b8cc
Joel <fallendemongod@gmail.com>
2022-10-02 16:01:51 -0400

    Added Animal Class

---
+++ Assets/Scripts/Animals/Animal.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class Animal : SerializedMonoBehaviour
+{
+    
+    
+    [SerializeField] private Animator anim;
+    [SerializeField] private AnimalSO animalInfo;
+    
+    
+}
