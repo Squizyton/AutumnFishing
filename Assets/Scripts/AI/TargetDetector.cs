c2427bb9465aa1ac4345c39ec148d3a7efa427b9
Joel <fallendemongod@gmail.com>
2022-10-02 22:01:22 -0400

    Added Target Detector

---
+++ Assets/Scripts/AI/TargetDetector.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using AI;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class TargetDetector : Detector
+{
+    private float targetDetectionRange = 8.24f;
+
+    [SerializeField] private LayerMask obstaclesLayer, playerLayerMask;
+
+
+    [Title("Debug Values")] [SerializeField]
+    private bool showGizmos;
+
+    [SerializeField] private List<Transform> colliders;
+
+    public override void Detect(AIData aiData)
+    {
+        //Find out if player is near
+        var playerCollider = Physics.OverlapSphere(transform.position, targetDetectionRange, playerLayerMask);
+
+
+        if (playerCollider.Length > 0)
+        {
+            
+            Debug.Log("Dick Tit Fuck McGuck");
+            var direction = (playerCollider[0].transform.position - transform.position).normalized;
+
+            Physics.Raycast(transform.position, direction, out var hit, targetDetectionRange, obstaclesLayer);
+
+            if (hit.collider && (playerLayerMask & (1 << hit.collider.gameObject.layer)) != 0)
+            {
+                Debug.DrawRay(transform.position, direction * targetDetectionRange, Color.magenta);
+                colliders = new List<Transform>() {playerCollider[0].transform};
+            }
+            else
+            {
+                Debug.Log("This is being called");
+                colliders = null;
+            }
+        }
+        else colliders = null;
+        
+        aiData.targets = colliders;
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        if (showGizmos == false)
+            return;
+
+        Gizmos.DrawWireSphere(transform.position, targetDetectionRange);
+
+        if (colliders == null)
+            return;
+        Gizmos.color = Color.magenta;
+        foreach (var item in colliders)
+        {
+            Gizmos.DrawSphere(item.position, 0.3f);
+        }
+    }
+}
