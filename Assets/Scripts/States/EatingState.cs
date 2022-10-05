3f8caf5b03403757cf679a0fdd9074045d3dda66
Joel <fallendemongod@gmail.com>
2022-10-05 17:24:33 -0400

    Created eating state

---
+++ Assets/Scripts/States/EatingState.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using States;
+using UnityEngine;
+
+public class EatingState : State
+{
+    public override void OnInitialized(Animal passedAnimal, AIData passedData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void FixedUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
