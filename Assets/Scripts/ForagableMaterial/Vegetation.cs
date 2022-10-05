cb753a5445e1a14379924017acbe497bda1f157f
Joel <fallendemongod@gmail.com>
2022-10-05 17:25:25 -0400

    Able to return player position and if isSprinting

--- Assets/Scripts/ForagableMaterial/Vegetation.cs
+++ Assets/Scripts/ForagableMaterial/Vegetation.cs
@@ -5,6 +5,7 @@ namespace ForagableMaterial
     [CreateAssetMenu(menuName = "New Fauna", fileName = "New Fauna")]
     public class Vegetation : ScriptableObject
     {
+        //Material name
         public string materialName;
         public GameObject prefab;
         public Sprite sprite;   
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,74 +1,78 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using Singleton;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-   [Title("Rigidbody")]
-   [SerializeField] private Rigidbody rb;
-
-   [Title("Movement")] 
-   [SerializeField,ReadOnly] private float currentSpeed;
-   [SerializeField] private float walkSpeed;
-   [SerializeField] private float runSpeed;
-   
-   
-
-   private Vector3 axis;
-   private State state;
-
-   private void Start()
+   public class PlayerMovement : SingletonBehaviour<PlayerMovement>
    {
-      currentSpeed = walkSpeed;
-   }
+      [Title("Rigidbody")]
+      [SerializeField] private Rigidbody rb;
 
+      [Title("Movement")] 
+      [SerializeField,ReadOnly] private float currentSpeed;
+      [SerializeField] private float walkSpeed;
+      [SerializeField] private float runSpeed;
+      public bool isSprinting;
+   
 
-   private void Update()
-   {
-      axis = state switch
+      private Vector3 axis;
+      private State state;
+
+      private void Start()
       {
-         State.canMove => new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized,
-         _ => axis
-      };
-      
-      if(Input.GetKeyDown(KeyCode.LeftShift))
-         currentSpeed = runSpeed;
-      else if(Input.GetKeyUp(KeyCode.LeftShift))
          currentSpeed = walkSpeed;
-   }
-   
-   private void FixedUpdate()
-   {
-      Movement();
-   }
+      }
 
 
-
-   private void Movement()
-   {
-      //If the player is actually moving
-      if(axis != Vector3.zero)
+      private void Update()
       {
+         axis = state switch
+         {
+            State.canMove => new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized,
+            _ => axis
+         };
+      
+         if(Input.GetKeyDown(KeyCode.LeftShift))
+            currentSpeed = runSpeed;
+         else if(Input.GetKeyUp(KeyCode.LeftShift))
+            currentSpeed = walkSpeed;
+      }
+   
+      private void FixedUpdate()
+      {
+         Movement();
+      }
+
+
+
+      private void Movement()
+      {
+         //If the player is actually moving
+         if(axis != Vector3.zero)
+         {
         
 //Move the player in the direction of the axis and the direction player is looking
-         rb.MovePosition(transform.position + (transform.forward * axis.z + transform.right * axis.x) * (currentSpeed * Time.deltaTime));
+            rb.MovePosition(transform.position + (transform.forward * axis.z + transform.right * axis.x) * (currentSpeed * Time.deltaTime));
 
-      }else
-      {
-         //If the player is not moving
-         //Stop the player
-         rb.velocity = Vector3.zero;
+         }else
+         {
+            //If the player is not moving
+            //Stop the player
+            rb.velocity = Vector3.zero;
+         }
       }
-   }
 
-   
-   
+      public Vector3 GetPosition()
+      {
+         return transform.position;
+      }
 
-   private enum State
-   {
-      canMove,
-      canNotMove
+
+      private enum State
+      {
+         canMove,
+         canNotMove
+      }
    }
 }
